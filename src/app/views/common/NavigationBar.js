6126a1b981697e03e42994e85b574cb8516bd51e
Fernando Alegría <fernanalegria@gmail.com>
2019-02-04 20:49:31 +0100

    feat: Activate nav links when their corresponding pages are displayed

--- src/app/views/common/NavigationBar.js
+++ src/app/views/common/NavigationBar.js
@@ -5,28 +5,40 @@ import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './NavigationBar.scss';
 
-const NavigationBar = props => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand>Would you rather?</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Link to="/questions" className="nav-link">
-        Questions
-      </Link>
-      <Link to="/add" className="nav-link">
-        New Question
-      </Link>
-      <Link to="/leaderboard" className="nav-link">
-        Leaderboard
-      </Link>
-    </Nav>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text>
-        Signed in as: <span className="authed-user">{props.user.name}</span>
-      </Navbar.Text>
-    </Navbar.Collapse>
-  </Navbar>
-);
+const NavigationBar = props => {
+  const { pathname } = props.location;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>Would you rather?</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Link
+          to="/questions"
+          className={`nav-link ${pathname.includes('questions') && 'active'}`}
+        >
+          Questions
+        </Link>
+        <Link
+          to="/add"
+          className={`nav-link ${pathname.includes('add') && 'active'}`}
+        >
+          New Question
+        </Link>
+        <Link
+          to="/leaderboard"
+          className={`nav-link ${pathname.includes('leaderboard') && 'active'}`}
+        >
+          Leaderboard
+        </Link>
+      </Nav>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <span className="authed-user">{props.user.name}</span>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 const mapStateToProps = ({ authedUser, users }) => ({
   user: users[authedUser]
