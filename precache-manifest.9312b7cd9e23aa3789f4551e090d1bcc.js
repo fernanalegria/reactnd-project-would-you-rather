992df29854203c812cd63c4a64cc122c3662ac78
Fernando Alegría <fernanalegria@gmail.com>
2019-02-16 13:44:21 +0100

    Updates

---
+++ precache-manifest.9312b7cd9e23aa3789f4551e090d1bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b19484326d78f9548c0",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/runtime~main.4b194843.js"
+  },
+  {
+    "revision": "7884c191db51b4490afa",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/main.7884c191.chunk.js"
+  },
+  {
+    "revision": "d0f16cfd49b2612a0068",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/2.d0f16cfd.chunk.js"
+  },
+  {
+    "revision": "7884c191db51b4490afa",
+    "url": "/reactnd-project-would-you-rather-starter/static/css/main.6444e430.chunk.css"
+  },
+  {
+    "revision": "d0f16cfd49b2612a0068",
+    "url": "/reactnd-project-would-you-rather-starter/static/css/2.9eaec080.chunk.css"
+  },
+  {
+    "revision": "a619d5e18f54f8b976275090d9d7d531",
+    "url": "/reactnd-project-would-you-rather-starter/index.html"
+  }
+];
