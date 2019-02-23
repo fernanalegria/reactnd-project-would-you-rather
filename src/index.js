cc3d345a16cb4a2f0e8edde7fa6d9c933e18f16f
Fernando Alegría <fernanalegria@gmail.com>
2019-02-23 21:06:25 +0100

    chore: Rename url address

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ const RootHtml = () => (
   </Provider>
 );
 
-export const rootUrl = '/reactnd-project-would-you-rather-starter';
+export const rootUrl = '/reactnd-project-would-you-rather';
 
 ReactDOM.render(<RootHtml />, document.getElementById('root'));
